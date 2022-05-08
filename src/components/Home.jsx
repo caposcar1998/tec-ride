d712c79b9ce7c7e19f93d31478e825413cb6ac3b
EmilianoPinedaTec <A01332517@tec.mx>
2022-05-08 18:10:57 -0500

    Home Terminada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,45 @@
+import imageBlockchain from "../images/blockchain.png"
+
+function Member(props){
+    return(
+        <div className="member">
+            <h3>{props.name}</h3>
+            <h3>{props.idM}</h3>
+        </div>
+    )
+}
+
 const Home = () => {
     return(
-        <h1>Hola</h1>
+        <div className="containerHome">
+            <h1>Rides Blockchain WebApp</h1>
+            <div className="contentHome1">
+                <img src={imageBlockchain} className="imgBlock"></img>
+                <div className="infoP">
+                    <p>
+                        In this project Blockchain technology will be used. Blockchain could be define as a chain 
+                        of blocks that contains information and the main porpuse of this is to make changes, 
+                        hacks or system cheat more difficult or even impossible.
+                    </p>
+                    <p>
+                        Why is it important to use blockchain in enterprises? Block is based on principles of cryptography, 
+                        decentralization and consensus which ensures trust in transactions. In most blockchains or distributed 
+                        ledger technologies (DLT), the data is structured into blocks and each block contains a transaction or 
+                        bundle of transactions. Each new block connects to all the blocks before it in a cryptographic chain in 
+                        such a way that it's nearly impossible to tamper with. All transactions within the blocks are validated 
+                        and agreed upon by a consensus mechanism, ensuring that each transaction is true and correct.
+                    </p>
+                </div>
+            </div>
+            <h2>Team Members</h2>
+            <div className="teamMembers">
+                <Member name="Manuel Ortiz" idM="A01655515"/>
+                <Member name="Aldo Ortiz" idM="A01654725"/>
+                <Member name="Óscar Contreras" idM="A01655772"/>
+                <Member name="Emiliano Pineda" idM="A01332517"/>
+            </div>
+            
+        </div>
     )
 }
 
