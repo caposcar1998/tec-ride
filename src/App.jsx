663fdd3d80890421ccaeb165b20be4f46a2f1f1b
Aldo Ortiz <aortizm@live.com.mx>
2022-05-31 23:03:21 -0500

    Rutas hacia "User Rides"
    Se agrega nueva ruta hacia "User Rides"

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
 import Offers from './components/Offers';
 import Travels from './components/Travels';
 import CreateRide from './components/CreateRide';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import MyRides from './components/MyRides';
-import { useEffect, useState } from 'react';
+import UserRides from './components/UserRides';
+import {useEffect, useState} from 'react';
 import Web3 from 'web3';
 import Register from './components/Register';
 
 function App() {
+	const [account, setAccount] = useState();
 
-  const [account, setAccount] = useState();
+	useEffect(() => {
+		async function load() {
+			const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+			const accounts = await web3.eth.requestAccounts();
 
-  useEffect(() => {
-    async function load() {
-      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-      const accounts = await web3.eth.requestAccounts();
-      
-      setAccount(accounts[0]);
-      localStorage.setItem('idUser', accounts[0])
-      localStorage.setItem('type','driver')
-    }
-    
-    load();
-   }, []);
+			setAccount(accounts[0]);
+			localStorage.setItem('idUser', accounts[0]);
+			localStorage.setItem('type', 'driver');
+		}
 
-  return (
-    <div className="App">
-      {account ?
-      <>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/offers" element={<Offers/>}/>
-        <Route path="/travels" element={<Travels/>}/>
-        <Route path='/offer-ride' element={<CreateRide/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/my-offers' element={<MyRides/>}/>
-      </Routes>
-      </>    
-    :
-    <Register/>
-    }
+		load();
+	}, []);
 
-      
-    </div>
-  )
+	return (
+		<div className="App">
+			{account ? (
+				<>
+					<NavBar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/offers" element={<Offers />} />
+						<Route path="/travels" element={<Travels />} />
+						<Route path="/offer-ride" element={<CreateRide />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<SignUp />} />
+						<Route path="/my-offers" element={<MyRides />} />
+						<Route path="/user-rides" element={<UserRides />} />
+					</Routes>
+				</>
+			) : (
+				<Register />
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,28 @@
-import { Link } from "react-router-dom"
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<Link to="/">
+				<button className="btn">Home</button>
+			</Link>
+			<Link to="/offers">
+				<button className="btn">Offers</button>
+			</Link>
+			<Link to="/travels">
+				<button className="btn">Travels</button>
+			</Link>
+			<Link to="/offer-ride">
+				<button className="btn">Offer ride</button>
+			</Link>
+			<Link to="/my-offers">
+				<button className="btn">My rides</button>
+			</Link>
+			<Link to="/user-rides">
+				<button className="btn">User rides</button>
+			</Link>
+		</nav>
+	);
+};
 
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/">
-                <button className="btn">
-                    Home
-                </button>
-            </Link>
-            <Link to="/offers">
-                <button className="btn">
-                    Offers
-                </button>
-            </Link>
-            <Link to="/travels">
-                <button className="btn">
-                    Travels
-                </button>
-            </Link>       
-            <Link to="/offer-ride">
-                <button className="btn">
-                    Offer ride
-                </button>
-            </Link>   
-            <Link to="/my-offers">
-            <button className="btn">
-                My rides
-            </button>
-            </Link> 
-        </nav>
-    )
-}
-
-export default NavBar
+export default NavBar;
